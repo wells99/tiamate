ad381edff14c1b94790cff18fc77c99c9bd8ec79
unknown <nagilapereira8896@digitalcollege.internal>
2024-11-14 11:41:59 -0300

    inicio de projeto

---
+++ tailwilnd.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+           "terracota": '#64221D',
+           " mostarda": "#F7AE1A",
+           "preto-medio": "#292625",
+           "marrom-escuro": "#3B2416",
+           "marrom-claro": "#A97744",
+           "nude": "#F5E7DA",
+          },
+      },
+    },
+    plugins: [],
+  }
